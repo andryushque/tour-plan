9c892d44fca0d05a36999a0cfe4179c3940fff76
andryushque <andrew.ustinov23@gmail.com>
2020-07-20 13:07:04 +0300

    Create: swiper-slider in hotel section

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 /*=== Swiper Slider ===*/
+var mySwiper = new Swiper(".swiper-container", {
+  loop: true,
+  speed: 500,
+
+  navigation: {
+    nextEl: ".slider-button--next",
+    prevEl: ".slider-button--prev",
+  },
+});
