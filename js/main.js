29abfcbd410eb64e00728205da62aff2732fc0b3
andryushque <andrew.ustinov23@gmail.com>
2020-07-31 11:44:11 +0300

    Create: modal form for feedback

--- js/main.js
+++ js/main.js
@@ -41,18 +41,19 @@ $(document).ready(function () {
   /*=== Modal Form ===*/
   var modalButton = $("[data-toggle=modal]");
   var closeModalButton = $(".modal__close");
-  var modalOverlay = $(".modal__overlay");
-  var modalDialog = $(".modal__dialog");
   modalButton.on("click", openModal);
   closeModalButton.on("click", closeModal);
 
   function openModal() {
-    modalOverlay.addClass("modal__overlay--visible");
-    modalDialog.addClass("modal__dialog--visible");
+    var targetModal = $(this).attr("data-href");
+    $(targetModal).find(".modal__overlay").addClass("modal__overlay--visible");
+    $(targetModal).find(".modal__dialog").addClass("modal__dialog--visible");
   }
 
   function closeModal(event) {
     event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
     modalOverlay.removeClass("modal__overlay--visible");
     modalDialog.removeClass("modal__dialog--visible");
   }
